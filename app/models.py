1c87727be367f45a1b9996e77c31872b1e1075d2
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-16 02:34:47 +0300

    Add subscriber model

--- app/models.py
+++ app/models.py
@@ -94,3 +94,16 @@ def get_comments(cls,id):
 
 def __repr__(self):
     return f"Comment{self.comment}"
+
+class Subscriber(db.Model):
+    __tablename__='subscribers'
+
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),unique=True,index=True)
+
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Subscriber {self.email}'
