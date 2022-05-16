0b416bcc01059ee17c44ca9a730995d10bd582ab
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-16 10:12:13 +0300

    coment out Roles model

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(db.Model, UserMixin):
     id=db.Column(db.Integer, primary_key=True)
     username=db.Column(db.String(255), unique=True, nullable=False)
     email=db.Column(db.String(255), unique=True, index = True, nullable=False)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    # role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
     password_secure=db.Column(db.String(255), nullable=False)
     bio=db.Column(db.String(255))
@@ -43,15 +43,15 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"User:{self.username}"
 
-class Role(db.Model):
-    __tablename__ = 'roles'
+# class Role(db.Model):
+#     __tablename__ = 'roles'
 
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
+#     id = db.Column(db.Integer,primary_key = True)
+#     name = db.Column(db.String(255))
+#     users = db.relationship('User',backref = 'role',lazy="dynamic")
 
-    def __repr__(self):
-        return f'User {self.name}'
+#     def __repr__(self):
+#         return f'User {self.name}'
         
 
 class Blog(db.Model):
