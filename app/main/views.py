922107ac09822d4968f6edc0044c3b327df63025
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-16 10:12:23 +0300

    remove commented out code

--- app/main/views.py
+++ app/main/views.py
@@ -119,14 +119,3 @@ def user_posts(username):
     blogs = Blog.query.filter_by(user=user).order_by(Blog.posted.desc()).paginate(page = page, per_page = 4)
     return render_template('userposts.html',blogs=blogs,user = user)
 
-    
-# @main.route('/user/<uname>/update/pic',methods= ['POST'])
-# @login_required
-# def update_pic(uname):
-#     user = User.query.filter_by(username = uname).first()
-#     if 'photo' in request.files:
-#         filename = photos.save(request.files['photo'])
-#         path = f'photos/{filename}'
-#         user.profile_pic_path = path
-#         db.session.commit()
-#     return redirect(url_for('main.profile',uname=uname))
